324b1191b60db3064a9a8c7e03908c26c0100405
Johannes Englisch <cyberjoe0815@hotmail.com>
2018-11-18 21:16:00 +0100

    status bar adjustments

--- dwm/scripts/mpd-status.sh
+++ dwm/scripts/mpd-status.sh
@@ -5,8 +5,8 @@ mpc_output="$(mpc -f "[%title%[ – %artist%]]|[%file%]")"
 status="$(echo "$mpc_output" | grep -e '^\[' | awk '{print $1}')"
 test -z "$status" && exit
 
-icon=""
-test "$status" = "[playing]" && icon="‣ "
+icon="⏸"
+test "$status" = "[playing]" && icon="♫"
 
 current_song="$(echo "$mpc_output" | head -n1 | cut -c1-35)"
 
