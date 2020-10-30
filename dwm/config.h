bd7d29eab5cef464fc0234c0d83868aba98c755a
Johannes Englisch <cyberjoe0815@hotmail.com>
2020-10-30 08:50:27 +0100

    dwm: config tweaks

--- dwm/config.h
+++ dwm/config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 8;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "DejaVu Sans:size=10" };
@@ -122,8 +122,7 @@ static Button buttons[] = {
 
 /* custom functions */
 
-static
-void
+static void
 togglefullscreen(const Arg *arg)
 {
 	if (!selmon->sel)
