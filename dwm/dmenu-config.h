56485c1ccbfd2e998f608d0775c52fbdbd152161
Johannes Englisch <johannes.englisch@t-online.de>
2022-04-18 13:30:36 +0200

    dmenu: colour scheme

--- dwm/dmenu-config.h
+++ dwm/dmenu-config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"sans:size=10"
+	"sans:size=11"
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -14,10 +14,27 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 // 	[SchemeSel] = { "#eeeeee", "#550077" },
 // 	[SchemeOut] = { "#000000", "#00ffff" },
 // };
+// // Lim
+// static const char *colors[SchemeLast][2] = {
+// 	/*     fg         bg       */
+// 	[SchemeNorm] = { "#222222", "#e6e6e6" },
+// 	[SchemeSel] = { "#eeeeee", "#6868da" },
+// 	[SchemeOut] = { "#000000", "#00ffff" },
+// };
+// // Breeze
+// static const char *colors[SchemeLast][2] = {
+// 	/*     fg         bg       */
+// 	[SchemeNorm] = { "#232627", "#eff0f1" },
+// 	[SchemeSel] = { "#fcfcfc", "#3daee9" },
+// 	[SchemeOut] = { "#000000", "#00ffff" },
+// };
+// Fluxbox: zimek_green
+// sel bg == #add07e to #718541 (mid #8faa5f)
+// norm bg == #f0efe9 to #bfbeb2 (mid #d7d6cd)
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#222222", "#e6e6e6" },
-	[SchemeSel] = { "#eeeeee", "#6868da" },
+	[SchemeNorm] = { "#232627", "#d7d6cd" },
+	[SchemeSel] = { "#e7e7e7", "#8faa5f" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
