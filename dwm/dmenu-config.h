70b358bae0bf37f65ef4289b5b8ca384fa375137
Johannes Englisch <johannes.englisch@t-online.de>
2022-07-09 12:53:58 +0200

    dmenu: played around with colour schemes

--- dwm/dmenu-config.h
+++ dwm/dmenu-config.h
@@ -14,6 +14,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 // 	[SchemeSel] = { "#eeeeee", "#550077" },
 // 	[SchemeOut] = { "#000000", "#00ffff" },
 // };
+
 // // Lim
 // static const char *colors[SchemeLast][2] = {
 // 	/*     fg         bg       */
@@ -21,6 +22,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 // 	[SchemeSel] = { "#eeeeee", "#6868da" },
 // 	[SchemeOut] = { "#000000", "#00ffff" },
 // };
+
 // // Breeze
 // static const char *colors[SchemeLast][2] = {
 // 	/*     fg         bg       */
@@ -28,15 +30,33 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 // 	[SchemeSel] = { "#fcfcfc", "#3daee9" },
 // 	[SchemeOut] = { "#000000", "#00ffff" },
 // };
-// Fluxbox: zimek_green
-// sel bg == #add07e to #718541 (mid #8faa5f)
-// norm bg == #f0efe9 to #bfbeb2 (mid #d7d6cd)
+
+// Breeze Dark
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#232627", "#d7d6cd" },
-	[SchemeSel] = { "#e7e7e7", "#8faa5f" },
+	[SchemeNorm] = { "#fcfcfc", "#2a2e32" },
+	[SchemeSel] = { "#fcfcfc", "#3daee9" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
+// // Fluxbox: zimek_green
+// // sel bg == #add07e to #718541 (mid #8faa5f)
+// // norm bg == #f0efe9 to #bfbeb2 (mid #d7d6cd)
+// static const char *colors[SchemeLast][2] = {
+// 	/*     fg         bg       */
+// 	[SchemeNorm] = { "#232627", "#d7d6cd" },
+// 	[SchemeSel] = { "#e7e7e7", "#8faa5f" },
+// 	[SchemeOut] = { "#000000", "#00ffff" },
+// };
+
+// // greybird panel
+// static const char *colors[SchemeLast][2] = {
+// 	/*     fg         bg       */
+// 	[SchemeNorm] = { "#fcfcfc", "#242424" },
+// 	[SchemeSel] = { "#fcfcfc", "#22558a" },
+// 	[SchemeOut] = { "#000000", "#00ffff" },
+// };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
